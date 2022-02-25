3bf4ec332a303dc07bf4d2084414a292bc336ea1
JackChilds <35969199+JackChilds@users.noreply.github.com>
2022-02-25 18:05:28 +0000

    added icons and changed meta description

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document () {
     return (
         <Html>
-            <Head />
+            <Head>
+                <meta name="theme-color" content="hsl(223, 14%, 10%)" />
+                <meta name="description" content="Jack Childs. A 15 year old, full stack developer from the UK." />
+            </Head>
             <body className="font-mono">
                 <Main />
                 <NextScript />
