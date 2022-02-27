6332851d47e23f2f69f896ccf1f8193d9329ed69
JackChilds <35969199+JackChilds@users.noreply.github.com>
2022-02-27 13:30:35 +0000

    Update contact_email.js

--- components/contact_email.js
+++ components/contact_email.js
@@ -1,7 +1,7 @@
 export default function ContactEmail() {
     // generates an encoded string and then at runtime a script decodes the string and displays the email address.
     // this is used in an effort to stop bots from reading the email address and sending spam emails.
-    const EMAIL = 'contact@morcreate.com';
+    const EMAIL = 'contact@morcreate.net';
 
     const encodeEmail = (e) => {
         const b = Buffer.from(e).toString('base64');
