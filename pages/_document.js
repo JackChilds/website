213e273f60397485ba9f5af9cb26fddaa8448d2e
JackChilds <35969199+JackChilds@users.noreply.github.com>
2022-03-03 19:07:19 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document () {
             <Head>
                 <meta name="theme-color" content="hsl(223, 14%, 10%)" />
                 <meta name="description" content="Jack Childs. A 15 year old, full stack developer from the UK." />
+
+
+                {/* Privacy friendly tracking code, uses https://counter.dev for analytics */}
+                <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jackchilds",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");</script>
             </Head>
             <body className="font-mono">
                 <Main />
