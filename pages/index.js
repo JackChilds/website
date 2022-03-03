6c59f9f01e9caf094f670a7c873daf4c0e96fb99
JackChilds <35969199+JackChilds@users.noreply.github.com>
2022-03-03 19:22:48 +0000

    fix

--- pages/index.js
+++ pages/index.js
@@ -85,6 +85,7 @@ export default function Home() {
 {
   /* Privacy friendly tracking code, uses https://counter.dev for analytics */ } 
 <Script strategy="afterInteractive"
+id="analytics-script"
 dangerouslySetInnerHTML={{
   __html: `sessionStorage.getItem("_swa")||0===document.referrer.indexOf(location.protocol+"//"+location.host)||fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jackchilds",utcoffset:"1"})),sessionStorage.setItem("_swa","1");`
 }} />
