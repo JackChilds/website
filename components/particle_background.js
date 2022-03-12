26ce7f18e4001c5032f53d6142fc40797df411d1
JackChilds <35969199+JackChilds@users.noreply.github.com>
2022-03-12 12:28:53 +0000

    fixed particle background overflowing x on windows

--- components/particle_background.js
+++ components/particle_background.js
@@ -1,8 +1,8 @@
 import jump from 'jump.js'
 export default function ParticleBackground () {
     return (
-<div className="w-screen h-screen absolute top-0 left-0 -z-10">
-  <div id="particles-container" className="w-screen h-screen bg-base-300"></div>
+<div className="h-screen absolute top-0 left-0 right-0 -z-10">
+  <div id="particles-container" className="w-full h-screen bg-base-300"></div>
 
   <div 
   className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] rounded-3xl bg-base-300 shadow-[0_0_5px_4px_rgba(10,10,10,0.3)] flex justify-center items-center text-center color-white p-8 cursor-pointer select-none" 
