2e9026379879228d052cc1aba1fe94df5bfde85e
Jack Childs <35969199+JackChilds@users.noreply.github.com>
2022-03-14 07:42:40 +0000

    change email address to new domain

--- components/contact_email.js
+++ components/contact_email.js
@@ -1,7 +1,7 @@
 export default function ContactEmail() {
     // generates an encoded string and then at runtime a script decodes the string and displays the email address.
     // this is used in an effort to stop bots from reading the email address and sending spam emails.
-    const EMAIL = 'contact@morcreate.net';
+    const EMAIL = 'contact@jackchilds.tech';
     const SUBJECT = 'I am interested in your work'
 
     const encodeEmail = (e) => {
